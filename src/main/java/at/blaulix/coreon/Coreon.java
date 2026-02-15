4df82fc121dbb1281892ed48cf9d5d339feae50e
Felix <blaulix@gmx.at>
2026-02-15 14:06:13 +0100

    Add initial project structure with .gitignore, Coreon plugin, and pom.xml

---
+++ src/main/java/at/blaulix/coreon/Coreon.java
@@ -0,0 +1,17 @@
+package at.blaulix.coreon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Coreon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
