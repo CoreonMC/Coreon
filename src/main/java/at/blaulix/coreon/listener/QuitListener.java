58a8f834d71bb24af0b3920f040a844df8893092
crqzycat <crqzycat@gmail.com>
2026-02-23 13:10:04 +0100

    QuitListener Comments

--- src/main/java/at/blaulix/coreon/listener/QuitListener.java
+++ src/main/java/at/blaulix/coreon/listener/QuitListener.java
@@ -1,21 +1,20 @@
 package at.blaulix.coreon.listener;
 
 import at.blaulix.coreon.util.Playerdata;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import java.io.File;
-import java.io.IOException;
-
 public class QuitListener implements Listener {
+
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
+
+        // Get the player who is leaving the server
         Player player = event.getPlayer();
 
+        // Save the player's data on disconnect
         Playerdata.savePlayerData(player);
     }
 }
