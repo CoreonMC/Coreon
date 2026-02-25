6b0b1a7922d8b03a33424e34f9a8368a208716a4
blaulix <blaulix@gmx.at>
2026-02-25 09:40:05 +0100

    Add player disappearance message in VanishHandler; improve player visibility management

--- src/main/java/at/blaulix/coreon/handler/VanishHandler.java
+++ src/main/java/at/blaulix/coreon/handler/VanishHandler.java
@@ -1,10 +1,15 @@
 package at.blaulix.coreon.handler;
 
+import org.bukkit.Color;
 import org.bukkit.entity.Player;
+
+import static org.bukkit.Bukkit.getServer;
 
 public class VanishHandler {
     public void vanishPlayer(Player player) {
+        Player allPlayer = getServer().getPlayer(player.getUniqueId());
         player.setInvisible(true);
         player.setSilent(true);
+        allPlayer.sendMessage(Color.YELLOW + "" + player + " left the game.");
     }
 }
